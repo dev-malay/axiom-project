f520dc89c15683a98e5dfd02aa39e28fe644f1b6
dev-malay <malayworkz@gmail.com>
2026-06-29 09:27:04 +0530

    refactor auth end to end

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const { register, login, getMe, updateProfile } = require("../controllers/auth.controller");
+const router = express.Router();
+const { register, login, getMe, updateProfile, googleSignIn } = require("../controllers/auth.controller");
 const { protect } = require("../middleware/auth.middleware");
 
-const router = express.Router();
+
+const authMiddleware = require("../middleware/auth.middleware")
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/google", googleSignIn);
+
 router.get("/me", protect, getMe);
-router.patch("/profile", protect, updateProfile);
+router.patch("/profile",protect, updateProfile);
 
 module.exports = router;
